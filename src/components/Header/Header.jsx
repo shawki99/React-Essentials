cf9c0c2b129d7da2e1a20b7911cb645e9f1cfcaf
shawki walid <shawkighorab@gmail.com>
2024-12-01 12:12:11 +0200

    seperating components for a cleaner structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,15 +7,15 @@ function getRandomInt(max) {
 }
 
 export default function Header() {
-    const description = reactDescriptions[getRandomInt(2)];
-    return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    );
-  }
+  const description = reactDescriptions[getRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
